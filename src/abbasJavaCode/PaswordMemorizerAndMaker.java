c0b3cf08c6bdd473f54e592b6355b11899be41d7
s490344 <s490344@FXH-9738868034>
2026-01-14 08:35:37 -0500

    yo

--- src/abbasJavaCode/PaswordMemorizerAndMaker.java
+++ src/abbasJavaCode/PaswordMemorizerAndMaker.java
@@ -8,10 +8,15 @@ import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.*;
+
 
 public class PaswordMemorizerAndMaker {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -53,17 +58,43 @@ public class PaswordMemorizerAndMaker {
 		JButton btnNewButton = new JButton("Create Password");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				System.out.print("\033[H\033[2J"); // ANSI escape codes for clearing screen (works in many terminals)
-				System.out.flush();
+			
+
 				
 			}
 		});
-		btnNewButton.setBounds(70, 100, 141, 93);
+		btnNewButton.setBounds(68, 255, 153, 33);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnGeussPassword = new JButton("Geuss Password");
-		btnGeussPassword.setBounds(286, 100, 141, 93);
+		btnGeussPassword.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+			}
+		});
+		btnGeussPassword.setBounds(297, 255, 170, 33);
 		frame.getContentPane().add(btnGeussPassword);
+		
+		textField = new JTextField();
+		textField.setBounds(80, 109, 107, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Put in your name:");
+		lblNewLabel_1.setBounds(80, 94, 107, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(80, 170, 107, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Your Password:");
+		lblNewLabel_1_1.setBounds(80, 158, 107, 14);
+		frame.getContentPane().add(lblNewLabel_1_1);
 	}
+	
+	
+	
+	
 }
